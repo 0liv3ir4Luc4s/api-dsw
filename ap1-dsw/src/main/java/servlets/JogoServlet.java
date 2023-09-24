e7e2ac5fdb405612f78435989e240ac3a452448c
Lucas Oliveira <0liv3ir4.luc4s@gmail.com>
2023-09-24 08:16:04 -0300

    feat: Add basic functionality (Classify number as odd or even)

---
+++ ap1-dsw/src/main/java/servlets/JogoServlet.java
@@ -0,0 +1,44 @@
+package servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class JogoServlet
+ */
+@WebServlet("/JogoServlet")
+public class JogoServlet extends HttpServlet {
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setCharacterEncoding("UTF-8");
+		
+		Integer n;
+		PrintWriter writer = response.getWriter();
+		
+		writer.write("<html>");
+		writer.write("<head><title>Sucesso</title>");
+		writer.write("<meta charset=\"UTF-8\">");
+		writer.write("<title>Jogo - API - DSW - Output</title></head>");
+		writer.write("<body>");
+		writer.write("<ul>");
+		for(int i = 1; i <= request.getParameterMap().values().size(); i++) {
+			n = Integer.parseInt(request.getParameter("n"+i));
+			writer.write("<li>");
+			writer.write("Número "+i+"("+n+")"+": "+(n % 2 == 0 ? "Par" : "Ímpar"));
+			writer.write("</li>");
+		}
+		writer.write("</ul>");
+		writer.write("<a href=\"index.html\">voltar</a>");
+		writer.write("</body>");
+		writer.write("</html>");
+	}
+
+}
