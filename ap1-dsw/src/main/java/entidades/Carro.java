1c4052b88a0c291496ea99faa64d2893380ba772
Lucas Oliveira <0liv3ir4.luc4s@gmail.com>
2023-09-24 21:41:45 -0300

    feat: Add insert screen and team members screen

---
+++ ap1-dsw/src/main/java/entidades/Carro.java
@@ -0,0 +1,35 @@
+package entidades;
+
+public class Carro {
+    private String modelo;
+    private String marca;
+    private Integer anoFabricacao;
+
+   
+    public Carro() {
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public Integer getAnoFabricacao() {
+        return anoFabricacao;
+    }
+
+    public void setAnoFabricacao(Integer anoFabricacao) {
+        this.anoFabricacao = anoFabricacao;
+    }
+}
